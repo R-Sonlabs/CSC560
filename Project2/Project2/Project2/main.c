b49c929d310c41ac87f660bbd1c481c35e116a06
Zirui Li <gracelizirui@gmail.com>
2019-03-12 01:37:23 -0700

    Add project 2

---
+++ Project2/Project2/Project2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project2.c
+ *
+ * Created: 3/12/2019 1:33:53 AM
+ * Author : grace
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
